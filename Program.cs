afd64f280b43f28ccc9ae556750b61484bc378a6
thanhdnh <myhoangthanh@yahoo.com>
2025-10-27 09:06:59 +0700

    OK

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
         //a = new int[5] { 2, 3, 5, 7, 1 };
         //a = new int[] { 2, 3, 5, 7, 1 };
 
+        /*
         int num = 10;
         int[] b = new int[num];
         Random r = new Random();
@@ -32,5 +33,69 @@
             if (b[i] % 2 == 0)
                 count++;
         Console.WriteLine("\nCount of even numbers: " + count);
+
+        //Sao chép mảng
+        int[] ar = new int[b.Length];
+        for (int i = 0; i < b.Length; i++)
+            ar[i] = b[i];
+        */
+        //Cách 1: Đếm số phần tử xuất hiện
+        int[] input = new int[] { 3, 9, 3, 3, 9, 1, 5 };
+        /*int[] freq = new int[input.Length];
+        for (int i = 0; i < input.Length; i++){
+            freq[i] = 0;
+            for (int j = 0; j < input.Length; j++){
+                if (input[i] == input[j])
+                    freq[i]++;
+            }
+        }
+        foreach(int x in input)
+            Console.Write(x + " ");
+        Console.WriteLine();
+        foreach (int x in freq)
+            Console.Write(x + " ");
+        */
+        //Cách 2:
+        //Stage 1: Sắp xếp
+        int[] sorted = new int[input.Length];
+        for (int i = 0; i < input.Length; i++)
+            sorted[i] = input[i];
+        for (int i = 0; i < sorted.Length - 1; i++)
+            for (int j = i + 1; j < sorted.Length; j++)
+                if (sorted[i] > sorted[j])
+                {
+                    int temp = sorted[i];
+                    sorted[i] = sorted[j];
+                    sorted[j] = temp;
+                }
+        int flag = 0;
+        for (int i = 0; i < sorted.Length-1; i++)
+            if (sorted[i + 1] != sorted[i])
+                flag++;
+        int[] frq = new int[flag+1];
+        int[] unique = new int[flag + 1];
+
+        flag = 0;
+        int index = 0;
+        for (int i = 0; i < sorted.Length-1; i++)
+        {
+            if (sorted[i + 1] != sorted[i])
+            {
+                unique[index] = sorted[i];
+                frq[index] = (i + 1) - flag;
+                flag = i + 1;
+                index++;
+            }
+            if (i+1 == sorted.Length - 1)
+            {
+                unique[index] = sorted[sorted.Length - 1];
+                frq[index] = sorted.Length - flag;
+            }
+        }
+        foreach (int x in unique)
+            Console.Write(x + " ");
+        Console.WriteLine();
+        foreach (int x in frq)
+            Console.Write(x + " ");
     }
 }
--- obj/Debug/net9.0/B04.AssemblyInfo.cs
+++ obj/Debug/net9.0/B04.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("B04")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+515497a1aee80ee872852d17dcc976b705a87e4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("B04")]
 [assembly: System.Reflection.AssemblyTitleAttribute("B04")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
